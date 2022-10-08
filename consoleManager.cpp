7f9a49478b2c4bcd18460c0950015fee63c953f1
romance <66000596+romance999@users.noreply.github.com>
2022-10-08 10:32:57 -0400

    center text slightly better

--- consoleManager.cpp
+++ consoleManager.cpp
@@ -11,7 +11,7 @@ void consoleManager::printf(string color, string text)
 
 void consoleManager::centerprintf(string color, string text)
 {
-	cout << setw(25); // modify the number to align it to the center
+	cout << setw(35); // modify the number to align it to the center
 	cout << "\033[" << color << "m" << text << endl; // should be centered now
 }
 
